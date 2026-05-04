2f990eee15aa1c3bc0b984b429c5b4854f36e215
jiaoyuan <imjiaoyuan@gmail.com>
2026-05-04 09:31:28 +0800

    release: version 0.1.2

--- src/jsrc/plot/__init__.py
+++ src/jsrc/plot/__init__.py
@@ -46,3 +46,24 @@ def register_subparser(subparsers):
     p.add_argument("-o", required=True, help="Output PNG file")
     p.add_argument("-dpi", type=int, default=300, help="DPI")
     p.set_defaults(func=_dispatch("jsrc.plot.cis"))
+
+    p = plot_sub.add_parser("heart", help="Plot heart curve")
+    p.set_defaults(func=_dispatch("jsrc.plot.heart"))
+
+    p = plot_sub.add_parser("rose", help="Plot 3D rose model")
+    p.set_defaults(func=_dispatch("jsrc.plot.rose"))
+
+    p = plot_sub.add_parser("dotplot", help="Sequence dotplot by exact k-mer matches")
+    p.add_argument("-fa1", required=True, help="FASTA file 1")
+    p.add_argument("-fa2", required=True, help="FASTA file 2")
+    p.add_argument("-k", type=int, default=10, help="k-mer size")
+    p.add_argument("-o", help="Optional output PNG")
+    p.add_argument("-dpi", type=int, default=300, help="DPI")
+    p.set_defaults(func=_dispatch("jsrc.plot.dotplot"))
+
+    p = plot_sub.add_parser("circoslite", help="Simple circular tracks for genome stats")
+    p.add_argument("-fa", required=True, help="Genome FASTA")
+    p.add_argument("-w", type=int, default=100000, help="Window size")
+    p.add_argument("-o", help="Optional output PNG")
+    p.add_argument("-dpi", type=int, default=300, help="DPI")
+    p.set_defaults(func=_dispatch("jsrc.plot.circoslite"))
